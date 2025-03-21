ad8ee3bd3baa3458c5f140a9f75f21d99f801555
yuki-js <me@aoki.app>
2025-03-21 17:56:31 +0900

    refactor: remove unused API route and fetch data in useEffect for index page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,26 +2,29 @@
 
 import { honoApp } from "@usagiverify/backend";
 import { hc } from "hono/client";
+import { useEffect } from "react";
 
 /**
  * Home component
  * @returns
  */
 export default function Page() {
-  const client = hc<typeof honoApp>("http://localhost:3000");
-  client.index
-    .$get({
-      json: {
-        name: "John Doe",
-        age: 30,
-      },
-    })
-    .then((res) => {
-      console.log(res);
-      console.log(res.status);
-      console.log(res.body);
-      console.log(res.headers);
-    });
+  useEffect(() => {
+    const client = hc<typeof honoApp>("http://localhost:3000");
+    client.index
+      .$get({
+        json: {
+          name: "John Doe",
+          age: 30,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        console.log(res.status);
+        console.log(res.body);
+        console.log(res.headers);
+      });
+  }, []);
 
   return <h1>Hello, Next.js!</h1>;
 }
