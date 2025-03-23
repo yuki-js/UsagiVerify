4ad90452d3e96659e0b46c55d8a8e7a7e3768c90
mashharuki <avp.104.106.107@ae.auone-net.jp>
2025-03-23 10:53:08 +0900

    NFTの機能を呼び出すためのメソッドをviemで実装しました。環境変数の設定やABIファイル、コントラクトのアドレスの設定を追加しています。

---
+++ backend/src/lib/viem.ts
@@ -0,0 +1,210 @@
+import * as dotenv from "dotenv";
+import {
+  createPublicClient,
+  createWalletClient,
+  getContract,
+  http,
+} from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { sepolia } from "viem/chains";
+import { ERC1155WithLock_ADDRESS } from "../util/constants";
+import { ERC1155WithLock_ABI } from "./../util/abi/ERC1155WithLock";
+
+dotenv.config();
+
+const { PRIVATE_KEY } = process.env;
+
+const chain = sepolia;
+
+// 公開クライアント
+const publicClient = createPublicClient({
+  chain,
+  transport: http(), // プロバイダーURLを指定
+});
+
+// ウォレットクライアント
+const walletClient = createWalletClient({
+  chain,
+  transport: http(),
+  account: privateKeyToAccount(PRIVATE_KEY as `0x${string}`),
+});
+
+// Create Contract instance
+const contract = getContract({
+  address: ERC1155WithLock_ADDRESS,
+  abi: ERC1155WithLock_ABI,
+  client: publicClient,
+});
+
+/**
+ * get token balance
+ * @param account
+ * @param tokenId
+ * @returns
+ */
+export const getBalance = async (account: string, tokenId: bigint) => {
+  return Number(await contract.read.balanceOf([account, tokenId]));
+};
+
+/**
+ * check if token is locked
+ * @param tokenId
+ * @returns
+ */
+export const isTokenLocked = async (tokenId: bigint) => {
+  return await contract.read.locked([tokenId]);
+};
+
+/**
+ * get token URI method
+ * @param tokenId
+ * @returns
+ */
+export const getTokenURI = async (tokenId: bigint) => {
+  return await contract.read.uri([tokenId]);
+};
+
+/**
+ * lock token method
+ * @param tokenId
+ */
+export const lockToken = async (tokenId: bigint) => {
+  try {
+    const txHash = await walletClient.writeContract({
+      address: ERC1155WithLock_ADDRESS,
+      abi: ERC1155WithLock_ABI,
+      functionName: "lock",
+      args: [tokenId],
+    });
+
+    // wait for tx to be mined
+    const receipt = await publicClient.waitForTransactionReceipt({
+      hash: txHash as `0x${string}`,
+    });
+
+    console.log("lockToken tx receipt:", receipt);
+  } catch (error) {
+    console.log("error occured when lockToken:", error);
+  }
+};
+
+/**
+ * token unlock method
+ * @param tokenId
+ */
+export const unlockToken = async (tokenId: bigint) => {
+  try {
+    const txHash = await walletClient.writeContract({
+      address: ERC1155WithLock_ADDRESS,
+      abi: ERC1155WithLock_ABI,
+      functionName: "unlock",
+      args: [tokenId],
+    });
+
+    // wait for tx to be mined
+    const receipt = await publicClient.waitForTransactionReceipt({
+      hash: txHash as `0x${string}`,
+    });
+
+    console.log("unlockToken tx receipt:", receipt);
+  } catch (error) {
+    console.log("error occured when unlockToken:", error);
+  }
+};
+
+/**
+ * mint NFT method
+ * @param to
+ * @param tokenId
+ * @param amount
+ * @param uri
+ */
+export const mintToken = async (
+  to: string,
+  tokenId: bigint,
+  amount: bigint,
+  uri: string
+) => {
+  try {
+    const txHash = await walletClient.writeContract({
+      address: ERC1155WithLock_ADDRESS,
+      abi: ERC1155WithLock_ABI,
+      functionName: "mint",
+      args: [to, tokenId, amount, uri],
+    });
+
+    // wait for tx to be mined
+    const receipt = await publicClient.waitForTransactionReceipt({
+      hash: txHash as `0x${string}`,
+    });
+
+    console.log("mintToken tx receipt:", receipt);
+  } catch (error) {
+    console.log("error occured when mint:", error);
+  }
+};
+
+/**
+ * simple transfer token method
+ * @param from
+ * @param to
+ * @param tokenId
+ * @param amount
+ * @returns
+ */
+export const transferToken = async (
+  from: string,
+  to: string,
+  tokenId: bigint,
+  amount: bigint
+) => {
+  try {
+    const txHash = await walletClient.writeContract({
+      address: ERC1155WithLock_ADDRESS,
+      abi: ERC1155WithLock_ABI,
+      functionName: "safeTransferFrom",
+      args: [from, to, tokenId, amount, "0x"],
+    });
+    // wait for tx to be mined
+    const receipt = await publicClient.waitForTransactionReceipt({
+      hash: txHash as `0x${string}`,
+    });
+
+    console.log("safeTransferFrom tx receipt:", receipt);
+  } catch (error) {
+    console.log("error occured when safeTransferFrom:", error);
+  }
+};
+
+/**
+ * batch transfer token method
+ * @param from
+ * @param to
+ * @param tokenIds
+ * @param amounts
+ * @returns
+ */
+export const transferBatchToken = async (
+  from: string,
+  to: string,
+  tokenIds: bigint[],
+  amounts: bigint[]
+) => {
+  try {
+    const txHash = await walletClient.writeContract({
+      address: ERC1155WithLock_ADDRESS,
+      abi: ERC1155WithLock_ABI,
+      functionName: "safeBatchTransferFrom",
+      args: [from, to, tokenIds, amounts, "0x"],
+    });
+
+    // wait for tx to be mined
+    const receipt = await publicClient.waitForTransactionReceipt({
+      hash: txHash as `0x${string}`,
+    });
+
+    console.log("safeBatchTransferFrom tx receipt:", receipt);
+  } catch (error) {
+    console.log("error occured when safeBatchTransferFrom:", error);
+  }
+};
